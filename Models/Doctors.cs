39ea2cd21983a4b2354ed61b86729c8c22a753d8
BhaveshKumarReddy <128818065+BhaveshKumarReddy@users.noreply.github.com>
2023-04-04 13:03:55 +0530

    Added MedicalRepSchedule Controller

---
+++ Models/Doctors.cs
@@ -0,0 +1,44 @@
+﻿namespace PharmacySupplyProject.Models
+{
+    public class Doctors
+    {
+        public Doctors() { }
+        List<Doctor> doctors = new List<Doctor>() {
+          new Doctor()
+          {
+              Name = "Aran",
+              ContactNumber = "9542289337",
+              TreatingAilment = ""
+          },
+          new Doctor()
+          {
+              Name = "Avinash",
+              ContactNumber = "8532280017",
+              TreatingAilment = ""
+          },
+          new Doctor()
+          {
+              Name = "Adarsh",
+              ContactNumber = "9232289112",
+              TreatingAilment = ""
+          },
+          new Doctor()
+          {
+              Name = "Pavith",
+              ContactNumber = "7569172719",
+              TreatingAilment = ""
+          },
+          new Doctor()
+          {
+              Name = "Parthe",
+              ContactNumber = "7169100701",
+              TreatingAilment = ""
+          }
+        };
+
+        public List<Doctor> getDoc()
+        {
+            return doctors;
+        }
+    }
+}
